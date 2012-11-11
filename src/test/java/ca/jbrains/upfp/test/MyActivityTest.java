c743e88da9c75d4443eae51ca9f72edc64628523
J. B. Rainsberger <me@jbrains.ca>
2012-11-11 11:37:18 -0400

    Finally got a basic IDEA-based project working for Android.

---
+++ src/test/java/ca/jbrains/upfp/test/MyActivityTest.java
@@ -0,0 +1,20 @@
+package ca.jbrains.upfp.test;
+
+import ca.jbrains.upfp.MyActivity;
+import ca.jbrains.upfp.R;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class MyActivityTest {
+
+    @Test
+    public void shouldHaveHappySmiles() throws Exception {
+        String appName = new MyActivity().getResources().getString(R.string.app_name);
+        assertThat(appName, equalTo("MyActivity"));
+    }
+}
