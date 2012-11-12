4aa46db2e09c2011748d8035eaccea56d7907265
J. B. Rainsberger <me@jbrains.ca>
2012-11-12 17:35:43 -0400

    Introduced class to allow the Activity to invoke an Export to CSV action

--- src/test/java/ca/jbrains/upfp/domain/test/ExportTransactionsToCsvTest.java
+++ src/test/java/ca/jbrains/upfp/domain/test/ExportTransactionsToCsvTest.java
@@ -63,12 +63,23 @@ public class ExportTransactionsToCsvTest {
 
 
     private void exportTransactionsToCsv(PrintWriter canvas, TransactionTextFormat transactionTextFormat, Iterable<Transaction> transactions) throws IOException {
-        // SMELL Different levels of abstraction between header and row
-        // Stops this from becoming more general-purpose
-        canvas.println("\"Date\",\"Category\",\"Amount\"");
-        for (Transaction each : transactions) {
-            canvas.println(transactionTextFormat.format(each));
-        }
+        new ExportTransactionsToCsvAction(transactionTextFormat).execute(transactions, canvas);
     }
 
+    public static class ExportTransactionsToCsvAction {
+        private final TransactionTextFormat transactionTextFormat;
+
+        public ExportTransactionsToCsvAction(TransactionTextFormat transactionTextFormat) {
+            this.transactionTextFormat = transactionTextFormat;
+        }
+
+        public void execute(Iterable<Transaction> transactions, PrintWriter canvas) {
+            // SMELL Different levels of abstraction between header and row
+            // Stops this from becoming more general-purpose
+            canvas.println("\"Date\",\"Category\",\"Amount\"");
+            for (Transaction each : transactions) {
+                canvas.println(transactionTextFormat.format(each));
+            }
+        }
+    }
 }
