163e6a89496ef125ae30476f270d13e2906ebc40
J. B. Rainsberger <me@jbrains.ca>
2012-11-12 15:25:01 -0400

    Separated writing a single transaction to CSV from the loop so that I could test it more thoroughly.

--- src/test/java/ca/jbrains/upfp/domain/test/ExportTransactionsToCsvTest.java
+++ src/test/java/ca/jbrains/upfp/domain/test/ExportTransactionsToCsvTest.java
@@ -1,13 +1,19 @@
 package ca.jbrains.upfp.domain.test;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import org.joda.time.LocalDate;
 import org.joda.time.YearMonthDay;
+import org.joda.time.format.DateTimeFormat;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Calendar;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -26,7 +32,7 @@ public class ExportTransactionsToCsvTest {
     public void oneTransaction() throws Exception {
         final StringWriter canvas = new StringWriter();
 
-        exportTransactionsToCsv(new PrintWriter(canvas), Lists.<Transaction>newArrayList(new TransactionBuilder().withDate(date(2012,11,17)).withCategory("Groceries").withAmount(-1350).build()));
+        exportTransactionsToCsv(new PrintWriter(canvas), Lists.<Transaction>newArrayList(new TransactionBuilder().withDate(date(2012, 11, 17)).withCategory("Groceries").withAmount(-1350).build()));
 
         assertEquals("\"Date\",\"Category\",\"Amount\"\n\"2012-11-17\",\"Groceries\",\"-13.50\"", canvas.toString().trim());
     }
@@ -35,14 +41,32 @@ public class ExportTransactionsToCsvTest {
         return new LocalDate(year, month, day);
     }
 
-    private void exportTransactionsToCsv(PrintWriter canvas, Iterable<Transaction> transactions) {
-        canvas.println("\"Date\",\"Category\",\"Amount\"");
-        if (transactions.iterator().hasNext())
-            canvas.println("\"2012-11-17\",\"Groceries\",\"-13.50\"");
+    private void exportTransactionsToCsv(PrintWriter canvas, Iterable<Transaction> transactions) throws IOException {
+        writeHeader(canvas);
+        for (Transaction each : transactions) {
+            writeTransaction(canvas, each);
+        }
     }
 
-    private static class Transaction {
+    private void writeTransaction(PrintWriter canvas, Transaction each) {
+        final String dateText = DateTimeFormat.forPattern("yyyy-MM-dd").print(each.date);
+        final double amountInDollars = each.amountInCents / 100.0d;
+        canvas.println(String.format("\"%1$s\",\"%2$s\",\"%3$.2f\"", dateText, each.categoryName, amountInDollars));
+    }
+
+    private void writeHeader(PrintWriter canvas) {
+        canvas.println("\"Date\",\"Category\",\"Amount\"");
+    }
+
+    public static class Transaction {
+        private final LocalDate date;
+        private final String categoryName;
+        private final int amountInCents;
+
         public Transaction(LocalDate date, String categoryName, int amountInCents) {
+            this.date = date;
+            this.categoryName = categoryName;
+            this.amountInCents = amountInCents;
         }
     }
 
