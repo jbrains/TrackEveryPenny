46c7765ce87eb2f51ce4c7ae69009ac3aa078574
J. B. Rainsberger <me@jbrains.ca>
2012-11-12 17:03:35 -0400

    We format any Transaction as a CSV row correctly.

--- src/test/java/ca/jbrains/upfp/ca/jbrains/upfp/domain/test/FormatTransactionAsCsvRowTest.java
+++ src/test/java/ca/jbrains/upfp/ca/jbrains/upfp/domain/test/FormatTransactionAsCsvRowTest.java
@@ -4,16 +4,47 @@ import ca.jbrains.upfp.domain.Transaction;
 import ca.jbrains.upfp.domain.TransactionCsvFormat;
 import ca.jbrains.upfp.domain.test.TransactionBuilder;
 import org.joda.time.LocalDate;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FormatTransactionAsCsvRowTest {
+    private LocalDate irrelevantDate;
+    private String irrelevantCategoryName;
+    private int irrelevantAmount;
+
+    @Before
+    public void setUp() throws Exception {
+        irrelevantDate = new LocalDate(2012, 11, 17);
+        irrelevantCategoryName = "irrelevant category";
+        irrelevantAmount = -1350;
+    }
+
     @Test
     public void happyPath() throws Exception {
-        final Transaction transaction = new TransactionBuilder().withDate(new LocalDate(2012, 11, 17)).withCategory("Groceries").withAmount(-1350).build();
-        assertEquals("\"2012-11-17\",\"Groceries\",\"-13.50\"", format(transaction));
+        final Transaction transaction = new TransactionBuilder().withDate(irrelevantDate).withCategory(irrelevantCategoryName).withAmount(irrelevantAmount).build();
+        assertEquals("\"2012-11-17\",\"irrelevant category\",\"-13.50\"", format(transaction));
     }
+
+    @Test
+    public void noDate() throws Exception {
+        final Transaction transaction = new TransactionBuilder().withCategory(irrelevantCategoryName).withAmount(irrelevantAmount).build();
+        assertEquals("\"\",\"irrelevant category\",\"-13.50\"", format(transaction));
+    }
+
+    @Test
+    public void noCategory() throws Exception {
+        final Transaction transaction = new TransactionBuilder().withDate(irrelevantDate).withAmount(irrelevantAmount).build();
+        assertEquals("\"2012-11-17\",\"\",\"-13.50\"", format(transaction));
+    }
+
+    @Test
+    public void noAmount() throws Exception {
+        final Transaction transaction = new TransactionBuilder().withDate(irrelevantDate).withCategory(irrelevantCategoryName).build();
+        assertEquals("\"2012-11-17\",\"irrelevant category\",\"0.00\"", format(transaction));
+    }
+
 
     private String format(Transaction transaction) {
         return new TransactionCsvFormat().format(transaction);
--- src/test/java/ca/jbrains/upfp/domain/TransactionCsvFormat.java
+++ src/test/java/ca/jbrains/upfp/domain/TransactionCsvFormat.java
@@ -4,8 +4,9 @@ import org.joda.time.format.DateTimeFormat;
 
 public class TransactionCsvFormat {
     public String format(Transaction transaction) {
-        final String dateText = DateTimeFormat.forPattern("yyyy-MM-dd").print(transaction.date);
+        final String dateText = (transaction.date == null) ? "" : DateTimeFormat.forPattern("yyyy-MM-dd").print(transaction.date);
         final double amountInDollars = transaction.amountInCents / 100.0d;
-        return String.format("\"%1$s\",\"%2$s\",\"%3$.2f\"", dateText, transaction.categoryName, amountInDollars);
+        final String categoryText = (transaction.categoryName == null) ? "" : transaction.categoryName;
+        return String.format("\"%1$s\",\"%2$s\",\"%3$.2f\"", dateText, categoryText, amountInDollars);
     }
 }
