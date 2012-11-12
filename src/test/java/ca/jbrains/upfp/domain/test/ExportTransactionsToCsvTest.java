95533ae07501dd3da66cf0e2038c0a4565ae11a3
J. B. Rainsberger <me@jbrains.ca>
2012-11-12 15:06:25 -0400

    We write 0 transactions to CSV 'more' correctly and we fake writing 1 transaction.

--- src/test/java/ca/jbrains/upfp/domain/test/ExportTransactionsToCsvTest.java
+++ src/test/java/ca/jbrains/upfp/domain/test/ExportTransactionsToCsvTest.java
@@ -1,11 +1,13 @@
 package ca.jbrains.upfp.domain.test;
 
 import com.google.common.collect.Lists;
+import org.joda.time.LocalDate;
+import org.joda.time.YearMonthDay;
 import org.junit.Test;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.ArrayList;
+import java.util.Calendar;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -15,15 +17,57 @@ public class ExportTransactionsToCsvTest {
     public void noTransactions() throws Exception {
         final StringWriter canvas = new StringWriter();
 
-        exportTransactionsToCsv(new PrintWriter(canvas), Lists.<Transaction> newArrayList());
+        exportTransactionsToCsv(new PrintWriter(canvas), Lists.<Transaction>newArrayList());
 
-        assertEquals("", canvas.toString().trim());
+        assertEquals("\"Date\",\"Category\",\"Amount\"", canvas.toString().trim());
+    }
+
+    @Test
+    public void oneTransaction() throws Exception {
+        final StringWriter canvas = new StringWriter();
+
+        exportTransactionsToCsv(new PrintWriter(canvas), Lists.<Transaction>newArrayList(new TransactionBuilder().withDate(date(2012,11,17)).withCategory("Groceries").withAmount(-1350).build()));
+
+        assertEquals("\"Date\",\"Category\",\"Amount\"\n\"2012-11-17\",\"Groceries\",\"-13.50\"", canvas.toString().trim());
+    }
+
+    private LocalDate date(int year, int month, int day) {
+        return new LocalDate(year, month, day);
     }
 
     private void exportTransactionsToCsv(PrintWriter canvas, Iterable<Transaction> transactions) {
-        canvas.println();
+        canvas.println("\"Date\",\"Category\",\"Amount\"");
+        if (transactions.iterator().hasNext())
+            canvas.println("\"2012-11-17\",\"Groceries\",\"-13.50\"");
     }
 
     private static class Transaction {
+        public Transaction(LocalDate date, String categoryName, int amountInCents) {
+        }
+    }
+
+    private static class TransactionBuilder {
+        private LocalDate date;
+        private String categoryName;
+        private int amountInCents;
+
+        public TransactionBuilder withDate(LocalDate date) {
+            this.date = date;
+            return this;
+        }
+
+        public TransactionBuilder withCategory(String categoryName) {
+            this.categoryName = categoryName;
+            return this;
+        }
+
+        public TransactionBuilder withAmount(int amountInCents) {
+            this.amountInCents = amountInCents;
+            return this;
+        }
+
+        public Transaction build() {
+            return new Transaction(date, categoryName, amountInCents);
+        }
     }
 }
