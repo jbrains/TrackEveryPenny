237feae213f138ba56305346137ae7a923c63d1e
J. B. Rainsberger <me@jbrains.ca>
2012-11-16 10:01:41 -0400

    Extracted contract tests for BrowseTransactionsModel.

---
+++ AndroidFree/src/test/java/ca/jbrains/upfp/model/test/BrowseTransactionsModelContract.java
@@ -0,0 +1,58 @@
+package ca.jbrains.upfp.model.test;
+
+import ca.jbrains.upfp.model.InternalStorageException;
+import ca.jbrains.upfp.mvp.BrowseTransactionsModel;
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public abstract class BrowseTransactionsModelContract {
+  @Test
+  public void zeroTransactions() throws Exception {
+    final BrowseTransactionsModel model
+        = createBrowseTransactionModelWith(
+        Lists.newArrayList());
+    assertEquals(
+        Collections.emptyList(),
+        model.findAllTransactions());
+    assertEquals(0, model.countTransactions());
+  }
+
+  @Test
+  public void manyTransactions() throws Exception {
+    final Collection<Object> transactions = Lists
+        .newArrayList(
+            new Object(), new Object(), new Object());
+    final InMemoryBrowseTransactionsModel model
+        = createBrowseTransactionModelWith(transactions);
+    assertEquals(transactions, model.findAllTransactions());
+    assertEquals(3, model.countTransactions());
+  }
+
+  @Test
+  public void findAllTransactionsFails() throws Exception {
+    final BrowseTransactionsModel model
+        = createFailingBrowseTransactionsModel(
+        new InternalStorageException());
+
+    try {
+      model.countTransactions();
+      fail(
+          "How did you count the transactions when you can't find them?!");
+    } catch (InternalStorageException success) {
+    }
+  }
+
+  protected abstract InMemoryBrowseTransactionsModel
+  createBrowseTransactionModelWith(
+      Collection<Object> transactions
+  );
+
+  protected abstract InMemoryBrowseTransactionsModel
+  createFailingBrowseTransactionsModel(
+      RuntimeException intentionalException
+  );
+}
--- AndroidFree/src/test/java/ca/jbrains/upfp/model/test/InMemoryBrowseTransactionsModelTest.java
+++ AndroidFree/src/test/java/ca/jbrains/upfp/model/test/InMemoryBrowseTransactionsModelTest.java
@@ -1,59 +1,19 @@
 package ca.jbrains.upfp.model.test;
 
-import ca.jbrains.upfp.model.InternalStorageException;
-import ca.jbrains.upfp.mvp.BrowseTransactionsModel;
-import com.google.common.collect.Lists;
-import org.junit.Test;
+import java.util.Collection;
 
-import java.util.*;
-
-import static org.junit.Assert.*;
-
-public class InMemoryBrowseTransactionsModelTest {
-  @Test
-  public void zeroTransactions() throws Exception {
-    final BrowseTransactionsModel model
-        = createBrowseTransactionModelWith(
-        Lists.newArrayList());
-    assertEquals(
-        Collections.emptyList(),
-        model.findAllTransactions());
-    assertEquals(0, model.countTransactions());
-  }
-
-  @Test
-  public void manyTransactions() throws Exception {
-    final Collection<Object> transactions = Lists
-        .newArrayList(
-            new Object(), new Object(), new Object());
-    final InMemoryBrowseTransactionsModel model
-        = createBrowseTransactionModelWith(transactions);
-    assertEquals(transactions, model.findAllTransactions());
-    assertEquals(3, model.countTransactions());
-  }
-
-  @Test
-  public void findAllTransactionsFails() throws Exception {
-    final BrowseTransactionsModel model
-        = createFailingBrowseTransactionsModel(
-        new InternalStorageException());
-
-    try {
-      model.countTransactions();
-      fail(
-          "How did you count the transactions when you can't find them?!");
-    } catch (InternalStorageException success) {
-    }
-  }
-
-  private InMemoryBrowseTransactionsModel createBrowseTransactionModelWith(
+public class InMemoryBrowseTransactionsModelTest
+    extends BrowseTransactionsModelContract {
+  @Override
+  protected InMemoryBrowseTransactionsModel createBrowseTransactionModelWith(
       Collection<Object> transactions
   ) {
     return new InMemoryBrowseTransactionsModel(
         transactions);
   }
 
-  private InMemoryBrowseTransactionsModel createFailingBrowseTransactionsModel(
+  @Override
+  protected InMemoryBrowseTransactionsModel createFailingBrowseTransactionsModel(
       final RuntimeException intentionalException
   ) {
 
