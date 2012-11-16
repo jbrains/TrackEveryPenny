2d9dbb4ea09049833b9d14416a661934f32900db
J. B. Rainsberger <me@jbrains.ca>
2012-11-16 09:59:12 -0400

    Removed references from the test methods to the implementation of BrowseTransactionsModel.

--- AndroidFree/src/test/java/ca/jbrains/upfp/model/test/InMemoryBrowseTransactionsModelTest.java
+++ AndroidFree/src/test/java/ca/jbrains/upfp/model/test/InMemoryBrowseTransactionsModelTest.java
@@ -1,6 +1,7 @@
 package ca.jbrains.upfp.model.test;
 
 import ca.jbrains.upfp.model.InternalStorageException;
+import ca.jbrains.upfp.mvp.BrowseTransactionsModel;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 
@@ -11,8 +12,8 @@ import static org.junit.Assert.*;
 public class InMemoryBrowseTransactionsModelTest {
   @Test
   public void zeroTransactions() throws Exception {
-    final InMemoryBrowseTransactionsModel model
-        = new InMemoryBrowseTransactionsModel(
+    final BrowseTransactionsModel model
+        = createBrowseTransactionModelWith(
         Lists.newArrayList());
     assertEquals(
         Collections.emptyList(),
@@ -26,20 +27,16 @@ public class InMemoryBrowseTransactionsModelTest {
         .newArrayList(
             new Object(), new Object(), new Object());
     final InMemoryBrowseTransactionsModel model
-        = new InMemoryBrowseTransactionsModel(transactions);
+        = createBrowseTransactionModelWith(transactions);
     assertEquals(transactions, model.findAllTransactions());
     assertEquals(3, model.countTransactions());
   }
 
   @Test
   public void findAllTransactionsFails() throws Exception {
-    final InMemoryBrowseTransactionsModel model
-        = new InMemoryBrowseTransactionsModel(null) {
-      @Override
-      public Collection<Object> findAllTransactions() {
-        throw new InternalStorageException();
-      }
-    };
+    final BrowseTransactionsModel model
+        = createFailingBrowseTransactionsModel(
+        new InternalStorageException());
 
     try {
       model.countTransactions();
@@ -47,5 +44,24 @@ public class InMemoryBrowseTransactionsModelTest {
           "How did you count the transactions when you can't find them?!");
     } catch (InternalStorageException success) {
     }
+  }
+
+  private InMemoryBrowseTransactionsModel createBrowseTransactionModelWith(
+      Collection<Object> transactions
+  ) {
+    return new InMemoryBrowseTransactionsModel(
+        transactions);
+  }
+
+  private InMemoryBrowseTransactionsModel createFailingBrowseTransactionsModel(
+      final RuntimeException intentionalException
+  ) {
+
+    return new InMemoryBrowseTransactionsModel(null) {
+      @Override
+      public Collection<Object> findAllTransactions() {
+        throw intentionalException;
+      }
+    };
   }
 }
