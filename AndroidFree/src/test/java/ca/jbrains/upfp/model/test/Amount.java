9b6fb8941764752097b2cbc5f1218a38ff57db21
J. B. Rainsberger <me@jbrains.ca>
2012-11-16 14:43:05 -0400

    Amount behaves as an immutable Whole Value.

--- AndroidFree/src/test/java/ca/jbrains/upfp/model/test/Amount.java
+++ AndroidFree/src/test/java/ca/jbrains/upfp/model/test/Amount.java
@@ -1,10 +1,32 @@
 package ca.jbrains.upfp.model.test;
 
-public class Amount {
+public final class Amount {
+  private final int cents;
+
   public Amount(int cents) {
+    this.cents = cents;
   }
 
-  static Amount cents(int cents) {
+  public static Amount cents(int cents) {
     return new Amount(cents);
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (other instanceof Amount) {
+      final Amount that = (Amount) other;
+      return this.cents == that.cents;
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return cents;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%1d cents", cents);
   }
 }
---
+++ AndroidFree/src/test/java/ca/jbrains/upfp/model/test/AmountValueObjectBehaviorTest.java
@@ -0,0 +1,50 @@
+package ca.jbrains.upfp.model.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AmountValueObjectBehaviorTest {
+  @Test
+  public void reflexive() throws Exception {
+    final Amount amount = Amount.cents(12);
+
+    assertEquals(amount, amount);
+  }
+
+  @Test
+  public void symmetric() throws Exception {
+    final Amount equalAmount = Amount.cents(20);
+    final Amount anotherEqualAmount = Amount.cents(20);
+    final Amount unequalAmount = Amount.cents(21);
+
+    assertEquals(equalAmount, anotherEqualAmount);
+    assertEquals(anotherEqualAmount, equalAmount);
+
+    assertFalse(equalAmount.equals(unequalAmount));
+    assertFalse(unequalAmount.equals(equalAmount));
+  }
+
+  @Test
+  public void transitive() throws Exception {
+    final Amount equalAmount1 = Amount.cents(32);
+    final Amount equalAmount2 = Amount.cents(32);
+    final Amount equalAmount3 = Amount.cents(32);
+    final Amount unequalAmount = Amount.cents(31);
+
+    assertEquals(equalAmount1, equalAmount2);
+    assertEquals(equalAmount2, equalAmount3);
+    assertEquals(equalAmount1, equalAmount3);
+
+    assertFalse(unequalAmount.equals(equalAmount1));
+    assertFalse(unequalAmount.equals(equalAmount2));
+  }
+
+  @Test
+  public void nothingEqualsNull() throws Exception {
+    assertFalse(Amount.cents(0).equals(null));
+  }
+
+  // I won't bother testing for consistency as long as
+  // Amount is immutable and final.
+}
