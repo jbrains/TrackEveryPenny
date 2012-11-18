c8fe284beab0b61980211827646ac8da2ef17e6b
J. B. Rainsberger <me@jbrains.ca>
2012-11-18 16:12:48 -0400

    Faked formatting a Transaction as a row of CSV text.

---
+++ AndroidFree/src/test/java/ca/jbrains/upfp/view/test/FormatTransactionAsCsvRowTest.java
@@ -0,0 +1,44 @@
+package ca.jbrains.upfp.view.test;
+
+import ca.jbrains.upfp.model.test.*;
+import com.google.common.base.*;
+import com.google.common.collect.*;
+import com.sun.istack.internal.Nullable;
+import org.joda.time.LocalDate;
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
+import static ca.jbrains.hamcrest.RegexMatcher.matches;
+import static org.junit.Assert.assertThat;
+
+public class FormatTransactionAsCsvRowTest {
+  @Test
+  public void happyPath() throws Exception {
+    final Transaction transaction = new Transaction(
+        new LocalDate(2012, 11, 14), new Category(
+        "Bowling Winnings"), Amount.cents(250));
+    final String rowText = formatTransactionAsCsvRow(
+        transaction);
+    assertThat(
+        rowText, matches(
+        Pattern.compile(
+            "\\s*\"2012-11-14\",\\s*\"Bowling Winnings\"," +
+            "\\s*\"2.50\"\\s*")));
+  }
+
+  private String formatTransactionAsCsvRow(
+      Transaction transaction
+  ) {
+    return Joiner.on(",").join(
+        Collections2.transform(
+            Lists.newArrayList(
+                "2012-11-14", "Bowling Winnings", "2.50"),
+            new Function<String, String>() {
+              @Override
+              public String apply(@Nullable String text) {
+                return "\"" + text + "\"";
+              }
+            }));
+  }
+}
