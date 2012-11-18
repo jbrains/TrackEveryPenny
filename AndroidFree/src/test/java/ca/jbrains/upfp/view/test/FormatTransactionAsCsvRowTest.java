b8853a8ecce48be90b1ba7c308a3b7659727fc10
J. B. Rainsberger <me@jbrains.ca>
2012-11-18 16:56:50 -0400

    Now when we check the Transaction CSV format, we don't care how formatting the date works.

--- AndroidFree/src/test/java/ca/jbrains/upfp/view/test/FormatTransactionAsCsvRowTest.java
+++ AndroidFree/src/test/java/ca/jbrains/upfp/view/test/FormatTransactionAsCsvRowTest.java
@@ -1,28 +1,43 @@
 package ca.jbrains.upfp.view.test;
 
 import ca.jbrains.upfp.model.test.*;
+import org.jmock.*;
+import org.jmock.integration.junit4.JMock;
 import org.joda.time.LocalDate;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.regex.Pattern;
 
 import static ca.jbrains.hamcrest.RegexMatcher.matches;
 import static org.junit.Assert.assertThat;
 
+@RunWith(JMock.class)
 public class FormatTransactionAsCsvRowTest {
+  private final Mockery mockery = new Mockery();
+  private final CsvFormat<LocalDate> dateFormat = mockery
+      .mock(CsvFormat.class, "date format");
   private final TransactionCsvFormat transactionCsvFormat
       = new TransactionCsvFormat(
-      new DateCsvFormat(), new CategoryCsvFormat(),
+      dateFormat, new CategoryCsvFormat(),
       new AmountCsvFormat());
 
   @Test
   public void happyPath() throws Exception {
+    mockery.checking(
+        new Expectations() {{
+          allowing(dateFormat).format(
+              with(
+                  any(
+                      LocalDate.class)));
+          will(returnValue("2012-11-14"));
+        }});
+
     final Transaction transaction = new Transaction(
         new LocalDate(2012, 11, 14), new Category(
         "Bowling Winnings"), Amount.cents(250));
     final String rowText = transactionCsvFormat
-        .formatTransactionAsCsvRow(
-            transaction);
+        .formatTransactionAsCsvRow(transaction);
     assertThat(
         rowText, matches(
         Pattern.compile(
