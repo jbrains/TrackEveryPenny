c5963961df0cd35da53db3892707230612ed47fb
J. B. Rainsberger <me@jbrains.ca>
2012-11-20 14:27:09 -0400

    Marked some duplication as incidental so that I don't tear my hair out trying to remove it.

--- src/test/java/ca/jbrains/upfp/view/android/test/DisplayNumberOfTransactionsTest.java
+++ src/test/java/ca/jbrains/upfp/view/android/test/DisplayNumberOfTransactionsTest.java
@@ -18,6 +18,8 @@ public class DisplayNumberOfTransactionsTest
     extends BrowseTransactionsViewContract {
   @Test
   public void happyPath() throws Exception {
+    // The duplication between this test setup and
+    // initializeView() is incidental; don't remove it
     final BrowseTransactionsActivity
         browseTransactionsActivity
         = new BrowseTransactionsActivity();
@@ -37,7 +39,9 @@ public class DisplayNumberOfTransactionsTest
         "12", transactionsCountView.getText().toString());
   }
 
-
+  // The duplication between initializeView() and the
+  // setup for the other tests is incidental; don't
+  // remove it
   @Override
   protected BrowseTransactionsView initializeView() {
     final BrowseTransactionsActivity
