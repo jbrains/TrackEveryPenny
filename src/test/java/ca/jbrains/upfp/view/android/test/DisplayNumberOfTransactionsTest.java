94e2b7f27421f9723a6e8e9c6728cc1fb02fdf39
J. B. Rainsberger <me@jbrains.ca>
2012-11-20 13:54:18 -0400

    Formatting changes.

--- src/test/java/ca/jbrains/upfp/view/android/test/DisplayNumberOfTransactionsTest.java
+++ src/test/java/ca/jbrains/upfp/view/android/test/DisplayNumberOfTransactionsTest.java
@@ -1,10 +1,10 @@
 package ca.jbrains.upfp.view.android.test;
 
 import android.widget.TextView;
-import ca.jbrains.upfp.BrowseTransactionsActivity;
-import ca.jbrains.upfp.R;
+import ca.jbrains.upfp.*;
 import ca.jbrains.upfp.view.BrowseTransactionsView;
-import ca.jbrains.upfp.view.test.BrowseTransactionsViewContract;
+import ca.jbrains.upfp.view.test
+    .BrowseTransactionsViewContract;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,23 +12,31 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
-public class DisplayNumberOfTransactionsTest extends BrowseTransactionsViewContract {
-    @Test
-    public void happyPath() throws Exception {
-        final BrowseTransactionsActivity browseTransactionsActivity = new BrowseTransactionsActivity();
-        browseTransactionsActivity.onCreate(null);
+public class DisplayNumberOfTransactionsTest
+    extends BrowseTransactionsViewContract {
+  @Test
+  public void happyPath() throws Exception {
+    final BrowseTransactionsActivity
+        browseTransactionsActivity
+        = new BrowseTransactionsActivity();
+    browseTransactionsActivity.onCreate(null);
 
-        browseTransactionsActivity.displayNumberOfTransactions(12);
+    browseTransactionsActivity.displayNumberOfTransactions(
+        12);
 
-        final TextView transactionsCountView = (TextView) browseTransactionsActivity
-                .findViewById(R.id.transactionsCount);
-        assertEquals("12", transactionsCountView.getText().toString());
-    }
+    final TextView transactionsCountView
+        = (TextView) browseTransactionsActivity
+        .findViewById(R.id.transactionsCount);
+    assertEquals(
+        "12", transactionsCountView.getText().toString());
+  }
 
-    @Override
-    protected BrowseTransactionsView initializeView() {
-        final BrowseTransactionsActivity browseTransactionsActivity = new BrowseTransactionsActivity();
-        browseTransactionsActivity.onCreate(null);
-        return browseTransactionsActivity;
-    }
+  @Override
+  protected BrowseTransactionsView initializeView() {
+    final BrowseTransactionsActivity
+        browseTransactionsActivity
+        = new BrowseTransactionsActivity();
+    browseTransactionsActivity.onCreate(null);
+    return browseTransactionsActivity;
+  }
 }
